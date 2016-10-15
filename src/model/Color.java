a0df02d020cd170862064cfd955236d69b845cc8
Rogusta <rogusta@kth.se>
2016-10-15 11:23:36 +0200

    Update Color.java

--- src/model/Color.java
+++ src/model/Color.java
@@ -12,7 +12,9 @@ package model;
 public enum Color {
     red,blue,green,purpil;
     
-    
+    /**
+     * @return a random enum
+     */
     public static Color getRandom(){
         return values()[(int) (Math.random()*values().length)];
     }
